e1dd247fbd6446034b9195b1d9417789911c8d24
Vladislavfite <117209322+Vladislavfite@users.noreply.github.com>
2025-11-01 01:00:45 +0500

    Refactor global variables declaration

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ const ENEMY_AGGRO = 150, TOWER_RANGE = 200;
 // =====================
 // 2. Глобальные переменные
 // =====================
-let towers = [];
 let enemies, towers, bullets, buildSprites, ui;
 let gold = START_GOLD;
 let wave = 0;
